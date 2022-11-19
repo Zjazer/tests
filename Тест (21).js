03349aa79883fd1ae976e262171197904a44d82c
Zjazer <115031085+Zjazer@users.noreply.github.com>
2022-11-19 02:50:16 -0800

    Add files via upload

---
+++ Тест (21).js
@@ -0,0 +1,40 @@
+1)
+function kub(a){
+    return a ** 3
+}
+console.log(test(2))
+console.log(test(3))
+console.log(test(4))
+console.log(test(5))
+
+2)
+let numbers = [2, 4, 67, 4]
+console.log(numbers[0])
+
+
+3)
+function tok(a, b) {
+    return a * b
+}
+  
+console.log(tok(20,8))
+console.log(tok(15,4))
+console.log(tok(18,8))
+
+4)
+function test(a) {
+        return a * 3600
+}
+
+console.log(test(2))
+console.log(test(3))
+console.log(test(4))
+
+5)
+function triangle(a, b) {
+    return a + b - 1
+}
+  
+console.log(triangle(10,8))
+console.log(triangle(15,4))
+console.log(triangle(5,8))
