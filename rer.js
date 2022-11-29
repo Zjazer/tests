3ccd9eca9bc04c9cc9b40dee87baf101e4db5e45
Zjazer <115031085+Zjazer@users.noreply.github.com>
2022-11-29 07:54:55 -0800

    урок 13 задание 2

---
+++ rer.js
@@ -0,0 +1,19 @@
+1
+let num = -4;
+console.log(Math.abs(num))
+
+2
+//верните кубический корень числа
+let num = 27;
+console.log(math.cbrt(num))
+
+
+3
+//округлите число к большему целому
+let num = 5.1;
+console.log(math.ceil(num))
+
+4
+//округлите число к меньшему целому
+let num = 5.9;
+console.log(math.floor(num))
