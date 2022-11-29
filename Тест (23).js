f3847ca535c082b41d701d5ee041adb125fc6aec
Zjazer <115031085+Zjazer@users.noreply.github.com>
2022-11-29 07:55:55 -0800

    test25

---
+++ Тест (23).js
@@ -0,0 +1,29 @@
+1)
+//верните абсолютное значение числа
+let num = -4;
+
+
+x = Math.abs(-4)
+console.log(x)
+2)
+//верните кубический корень числа
+let num = 27;
+
+
+x = Math.cbrt(27)
+console.log(x)
+
+3)
+//округлите число к большему целому
+let num = 5.1;
+
+x = Math.ceil(5.1)
+console.log(x)
+
+4)
+//округлите число к меньшему целому
+let num = 5.9;
+
+
+x = Math.floor(5.9)
+console.log(x)
