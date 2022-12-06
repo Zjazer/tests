8634c9ed19e81d60664addf31c67937bcdc910b3
Zjazer <115031085+Zjazer@users.noreply.github.com>
2022-12-06 08:19:26 -0800

    test56

---
+++ test56.js
@@ -0,0 +1,147 @@
+1
+//что выведет в консоли console.log(Boolean(10 > 9))
+
+true
+2)
+//что выведет в консоли console.log(Boolean(10 < 9))
+
+false
+3)
+//что выведет в консоли скрипт ниже
+if (1 == 2) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+false
+4)
+//что выведет в консоли скрипт ниже
+if (1 < 2) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+true
+5)
+//что выведет в консоли скрипт ниже
+if (1 > 2) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+
+false
+6)
+//что выведет в консоли скрипт ниже
+let x = 0;
+console.log(Boolean(x));
+
+false
+7)
+//что выведет в консоли скрипт ниже
+let x = -0;
+console.log(Boolean(x));
+
+false
+
+8)
+//что выведет в консоли скрипт ниже
+let x = "";
+console.log(Boolean(x));
+
+false
+9)
+//что выведет в консоли скрипт ниже
+let x;
+console.log(Boolean(x));
+
+false
+10)
+//что выведет в консоли скрипт ниже
+let x = "";
+console.log(Boolean(x));
+
+false
+11)
+//что выведет в консоли скрипт ниже
+let x = false;
+console.log(Boolean(x));
+
+false
+
+12)
+//что выведет в консоли скрипт ниже
+let x = 10/"H";
+console.log(Boolean(x));
+
+false
+
+13)
+//перечислите логические операторы
+===,>,<
+14)
+//что выведет в консоли скрипт ниже
+if (1 > 2 && 2 < 3 || "hello".length < 4) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+false
+15)
+//что выведет в консоли скрипт ниже
+if (1 > 2 && 2 < 3 || "hello".length < 4) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+false
+
+16)
+//что выведет в консоли скрипт ниже
+if (1 > 2 || 2 < 3 || "hello".length != 4) {
+    console.log(true);
+} else {
+    console.log(false);
+}
+
+true
+17)
+//создайте структуру ниже с помощью if, else, else if
+let money = 10;
+/*
+ если money больше или равно 5-ти, то вывести в консоли 'Я куплю пирог',
+ иначе если money меньше 5-ти и больше 1, то вывести в консоли 'Мало денег',
+ иначе вывести в консоли 'Денег нет'
+*/
+
+18)
+//превратите скрипт ниже в switch
+switch day = (new Date().getDay());{
+    case 1 
+       text = monday ;
+       break
+    
+    case 2 
+    text = tuesday ;
+    break;
+    case 3 
+    text = wednesday ;
+    break
+     case 4 
+        text = thursday ;
+       break;
+     case 5 
+     text = friday ;
+     break;
+     else 
+        text = Break time;
+    }
+    
+    
